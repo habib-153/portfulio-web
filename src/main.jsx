581aff7e75a6c09c9860cdc2e6d19b5d20ea4a30
habib-153 <h.r.sihab155@gmail.com>
2024-03-13 01:16:13 +0600

    routing fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,8 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home/Home/Home";
 import { Toaster } from "react-hot-toast";
-import ContactMe from "./Home/ContactMe/ContactMe";
 import Main from "./LayOut/Main";
-import AboutMe from "./Home/AboutMe/AboutMe";
-import Skills from "./Home/Skills/Skills";
-import Projects from "./Home/Projects/Projects";
+
 
 const router = createBrowserRouter([
   {
@@ -19,22 +16,22 @@ const router = createBrowserRouter([
         path: "/",
     element: <Home></Home>,
       },
-      {
-        path: "/aboutMe",
-    element: <AboutMe></AboutMe>,
-      },
-      {
-        path: "/skills",
-    element: <Skills></Skills>,
-      },
-      {
-        path: "/projects",
-    element: <Projects></Projects>,
-      },
-      {
-        path:'/contactMe',
-        element:<ContactMe></ContactMe>
-      }
+    //   {
+    //     path: "/aboutMe",
+    // element: <AboutMe></AboutMe>,
+    //   },
+    //   {
+    //     path: "/skills",
+    // element: <Skills></Skills>,
+    //   },
+    //   {
+    //     path: "/projects",
+    // element: <Projects></Projects>,
+    //   },
+    //   {
+    //     path:'/contactMe',
+    //     element:<ContactMe></ContactMe>
+    //   }
     ]
   },
 ]);
