4ee4544e5a4ef77abb2a204fbeea57055c8ab8ce
habib-153 <h.r.sihab155@gmail.com>
2024-05-23 16:15:45 +0600

    fix: responsive issue on contact

--- src/Home/ContactMe/ContactMe.jsx
+++ src/Home/ContactMe/ContactMe.jsx
@@ -4,7 +4,7 @@ import emailjs from "emailjs-com";
 import toast from "react-hot-toast";
 import { FaWhatsapp } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-
+import './custom.css'
 function ContactMe() {
   const [formData, setFormData] = useState({
     from_name: "",
@@ -14,9 +14,9 @@ function ContactMe() {
     reply_to: "",
   });
 
-  const serviceId = import.meta.env.VITE_SERVICE_ID
-  const templateId = import.meta.env.VITE_TEMPLATE_ID
-  const userId = import.meta.env.VITE_USER_ID
+  const serviceId = import.meta.env.VITE_SERVICE_ID;
+  const templateId = import.meta.env.VITE_TEMPLATE_ID;
+  const userId = import.meta.env.VITE_USER_ID;
 
   const handleChange = (e) => {
     setFormData({
@@ -67,51 +67,66 @@ function ContactMe() {
       >
         Contact Me
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
-        <div>
-          <h2 className="font-medium text-xl mb-2">Lets Contact with me</h2>
-          <div className="flex justify-between">
-            <div 
-              data-aos="fade-up"
-              data-aos-duration="2000"
-              className="badge-base LI-profile-badge"
-              data-locale="en_US"
-              data-size="large"
-              data-theme="light"
-              data-type="HORIZONTAL"
-              data-vanity="habiburrahman153"
-              data-version="v1"
-            >
-              <a
-                className="badge-base__link LI-simple-link"
-                href="https://bd.linkedin.com/in/habiburrahman153?trk=profile-badge"
-              ></a>
-            </div>
-            <div className="flex flex-col  gap-6">
-              <div className="border border-[#DCDCDC] rounded-2xl">
-                <div className="bg-[#e9e5df] flex items-center gap-1 text-[#075E54]  font-semibold rounded-t-xl p-3">
-                  WhatsApp
-                  <FaWhatsapp />
-                </div>
-                <div className="p-4 font-medium">
-                  <p>+8801575632219</p>
-                </div>
+      <div className="flex flex-wrap gap-4 my-4">
+        <div className="flex flex-col sm:flex-row gap-3">
+          <div 
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            className="badge-base LI-profile-badge hidden md:block"
+            data-locale="en_US"
+            data-size="medium"
+            data-theme="light"
+            data-type="HORIZONTAL"
+            data-vanity="habiburrahman153"
+            data-version="v1"
+          >
+            <a
+              className="badge-base__link LI-simple-link"
+              href="https://bd.linkedin.com/in/habiburrahman153?trk=profile-badge"
+            ></a>
+          </div>
+          <div 
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            className="badge-base LI-profile-badge md:hidden"
+            data-locale="en_US"
+            data-size="large"
+            data-theme="light"
+            data-type="HORIZONTAL"
+            data-vanity="habiburrahman153"
+            data-version="v1"
+          >
+            <a
+              className="badge-base__link LI-simple-link"
+              href="https://bd.linkedin.com/in/habiburrahman153?trk=profile-badge"
+            ></a>
+          </div>
+          <div className="flex flex-wrap md:flex-col  gap-6">
+            <div data-aos="fade-up"
+                data-aos-duration="1000" className="border border-[#DCDCDC] w-full custom rounded-2xl">
+              <div className="bg-[#e9e5df] flex items-center gap-1 text-[#075E54]  font-semibold rounded-t-xl p-3">
+                WhatsApp
+                <FaWhatsapp />
               </div>
-              <div className="border border-[#DCDCDC] rounded-2xl">
-                <div className="bg-[#f2a50c5d] flex items-center gap-1 text-[#c71610]  font-semibold rounded-t-xl p-3">
-                  Email
-                  <MdEmail />
-                </div>
-                <div className="p-4 font-medium">
-                  <a href="mailto:h.r.sihab155@gmail.com">
-                    h.r.sihab155@gmail.com
-                  </a>
-                </div>
+              <div className="p-4 font-medium">
+                <p>+8801575632219</p>
+              </div>
+            </div>
+            <div data-aos="fade-up"
+                data-aos-duration="1000" className="border w-full custom border-[#DCDCDC]  rounded-2xl">
+              <div className="bg-[#f2a50c5d] flex items-center gap-1 text-[#c71610]  font-semibold rounded-t-xl p-3">
+                Email
+                <MdEmail />
+              </div>
+              <div className="p-4 font-medium">
+                <a href="mailto:h.r.sihab155@gmail.com">
+                  h.r.sihab155@gmail.com
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div>
+        <div className="flex-1">
           <form className="space-y-3" onSubmit={onSubmit}>
             <div className="form-control">
               <input
@@ -128,7 +143,7 @@ function ContactMe() {
             <div className="form-control">
               <input
                 data-aos="fade-up"
-                data-aos-duration="300"
+                data-aos-duration="1000"
                 type="email"
                 name="reply_to"
                 placeholder="Your Email"
@@ -139,8 +154,8 @@ function ContactMe() {
             </div>
             <div className="form-control">
               <input
-                data-aos="fade-down"
-                data-aos-duration="300"
+                data-aos="fade-up"
+                data-aos-duration="1000"
                 type="text"
                 name="subject"
                 placeholder="Topic/Subject"
@@ -151,8 +166,8 @@ function ContactMe() {
             </div>
             <div className="form-control">
               <textarea
-                data-aos="fade-down"
-                data-aos-duration="300"
+                data-aos="fade-up"
+                data-aos-duration="1000"
                 name="message"
                 className="textarea textarea-primary  rounded-md w-full"
                 placeholder="Write your message here"
@@ -163,7 +178,7 @@ function ContactMe() {
             <div className="w-full text-center">
               <button
                 data-aos="fade-up"
-                data-aos-duration="300"
+                data-aos-duration="1000"
                 className="btn btn-outline text-[17px] border-cyan-500 hover:bg-[#0db3eae1]"
               >
                 Submit
